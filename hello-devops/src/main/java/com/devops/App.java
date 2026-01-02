26c24d8087b6b819f12a7eea7b638450764c07e9
laptop <2003mohaben@gmail.com>
2026-01-03 00:09:34 +0100

    modify jenkinsfile pour les notif slack

--- hello-devops/src/main/java/com/devops/App.java
+++ hello-devops/src/main/java/com/devops/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Bonjour et bon courage dans votre projet en DevOps this is dev branch." );
+        System.out.println( "Bonjour et bon courage dans votre projet en DevOps this is dev branch  ." );
     }
 }
